33f44f518f369598463b0336b9cf8576394fa770
user <email>
2019-07-02 23:42:11 +0200

    more refactoring

--- lib/tooling/generate-r-packages-json.R
+++ lib/tooling/generate-r-packages-json.R
@@ -1,3 +1,12 @@
+#Ghidra needed a plugins argument because the files had to be part of the derivation, r has rwrapper because env vars suffice. how should i integrate this into rootedoverlay?
+
+#Docu: r-pkgs.had.co.nz/description.html - consider writing somehting in Nix for the debian control format?
+# use dcf stuff?: https://stat.ethz.ch/R-manual/R-devel/RHOME/library/tools/html/writePACKAGES.html
+# !!!! probably should use https://github.com/r-lib/desc
+#lst <- loadMirrorList(); m <- chooseMirror("cran", lst); ww <- getManifests(m, lst); genNix(m, Sys.Date()-1, "3.8", ww$these[1:1,], ww$known) 
+#TODO consider ifelse in parts (how is it different from if expressions?)
+# ! https://github.com/r-lib/desc/blob/c860e7b2c42a00e43195d86215b081a2dac1805a/R/deps.R#L82
+
 #!/usr/bin/env Rscript
 library(data.table)
 library(jsonlite)
@@ -35,14 +44,15 @@ readFile <- function(fileName){
 
 #main function
 generateDB <- function(mirror){
-  chooseMirror(mirror, loadMirrors()) %>%
+  commandArgs(trailingOnly=TRUE) %>%
+    chooseMirror(loadMirrorList()) %>%
     getManifests(mirrors) %>% 
-    addHashes(mirror, mirrors) %>%
+    addHashes(mirror) %>%
     writeDB(mode="nix")
 }
 
 #Load mirror tuples from file
-loadMirrors <- function(){
+loadMirrorList <- function(){
   snapshotDate <- Sys.Date() - 1
   data <- jsonlite::fromJSON(paste0(sourceDir, "/mirrors.json"))
   # Variable substitution like this is kind of bad, but I didnt want to make
@@ -51,15 +61,15 @@ loadMirrors <- function(){
 }
 
 #process commandline args
-chooseMirror <- function(mirror, mirrors){
-  #mirrorType <- "cran"# commandArgs(trailingOnly=TRUE)[1]
-  stopifnot(mirror %in% names(mirrors))
-  mirror
+chooseMirror <- function(args, mirrors){
+  mirrorName <- args[1]
+  stopifnot(mirrorName %in% names(mirrors))
+  list(name=mirrorName, url=mirrors[[mirrorName]])
 }
 
 #TODO split knownpackages pipeline (is it even necessary) and the remote stuff
 #TODO what is the relationship between pkgs and knownpackages
-getManifests <- function(mirror, mirrors){
+getManifests <- function(mirror, mirrors){ #TODO
   #man i have no idea wtf is going on in this part
   write(paste("downloading package lists"), stderr())
   #TODO why are you downloading them all if you only use one? (or does it actually use more than one?) <- i.e. knownpackages
@@ -67,30 +77,39 @@ getManifests <- function(mirror, mirrors){
   l <- function(url) as.data.table(available.packages(url, filters=c("R_version", "OS_type", "duplicates"), method="libcurl"))
   allPackages <- lapply(mirrors, l)
   
-  thesePkgs <- allPackages[[mirror]][order(Package)] #data.table magic, knows Package is a column of...
+  thesePkgs <- allPackages[[mirror$name]][order(Package)] #data.table magic, knows Package is a column of...
   setkey(thesePkgs, Package) #this makes no sense...?
   
-  nixEscapeAttr <- function(x) gsub(pattern=".", replacement="_", x, fixed=TRUE)
   knownPackages <- unique(rbindlist(allPackages)$Package) #maybe knownpackages shouldnt look at the global set?, how can we hadle individual package sets on the nix side if yet
-  knownPackages <- sapply(knownPackages, nixEscapeAttr) #only seems to be used for depends?
+  #knownPackages <- sapply(knownPackages, nixEscapeAttr) #only seems to be used for depends? #TODO wtf this uses escapeattr but is thesepackages not escaped when i use it in the other place?
   
-  list(allPackages = allPackages, thesePkgs = thesePkgs, knownPackages = knownPackages)
+  list(allPackages = all, these = thesePkgs, known = knownPackages)
 }
 
 #TODO return type is weird
-genAllHashes <- function(pkgs, mirror, mirrors){ #Todo create packages list object tuple to pass around
-  write(paste("updating", mirror, "packages"), stderr())
-  pkgs$sha256 <- datmap(pkgs, function(p) nixprefetchcached(p$Package, p$Version, mirror, mirrors))
+addHashes <- function(pkgs, mirror, mode="nix"){ #Todo create packages list object tuple to pass around
+  write(paste("updating", mirror$name, "packages"), stderr())
+
+  cacheFetcher <- if (mode == "nix"){ nixprefetchcached } else { jsonprefetchcached }
+  
+  lambda <- function(p) {
+    result <- cacheFetcher(p$Package, p$Version, mirror) #TODO just try all modes? what if theres multiple successful? - no tthat that will ever happen right -- hm, so error., ! also add script version to output format_semver
+    if (rlang::is_empty(result)){ 
+      result <- genHash(p$Package, p$Version, mirror)
+    }
+    result
+  }
+  pkgs$sha256 <- datmap(pkgs, lambda)
   write("done", stderr())  
   pkgs
 }
 
 #TODO use a temporary store ?
-genNixHash <- function(name, version, mirror, mirrors) {
+genHash <- function(name, version, mirror) {
   write(sprintf("fetching %s : %s", name, version), stderr())
   
   # TODO "avoid nix-prefetch-url because it often fails to fetch/hash large files" is this still true, also jeez how big can R stuff be?
-  url <- sprintf("%s%s_%s.tar.gz", mirrors[[mirror]], name, version)
+  url <- sprintf("%s%s_%s.tar.gz", mirror$url, name, version)
   tmp <- tempfile(pattern = sprintf("%s_%s", name, version), fileext=".tar.gz")
   
   wget <-    "wget -q -O '%1$s' '%2$s'" # tmp, url
@@ -99,4 +118,33 @@ genNixHash <- function(name, version, mirror, mirrors) {
   rm <-    "; rm -rf '%1$s'" # tmp
   cmd <- sprintf(paste(wget, hash, echo, rm, collapse=" "), tmp, url, name, version)
   system(cmd, intern=TRUE)
+}
+
+#used by toDeps
+#not sure which answer here we need https://stackoverflow.com/questions/21567057/programmatically-get-list-of-base-packages
+getKnownBasePackages <- function() {
+  #TODO well, R usually has a version constraint when its mentioned in deps
+  c("R", rownames(installed.packages(priority="base")))
+}
+
+#TODO generator wrapper script: since we query the environment we need to use the proper version of r to generate things? -> TODO document what parts of the env we query, so far its just this?
+#TODO add tpye signature -ish comments
+
+
+#TODO todo consider figuring out how to call idesc_get_deps, it sets the first ("type") argument parse_deps properly
+#TODO consider handling version range assertions
+toDeps <- function(deps, knownPackages){
+  fromNA <- function(s) ifelse(is.na(s), "",s)
+  renameKeywords <- function(d) ifelse(d == "import", "r_import", d)
+  
+  packages <- desc:::parse_deps("wat", paste(lapply(deps, fromNA), collapse=","))$package
+  
+  #TODO probably need to run a bootstrap step - but actually that should be unnecessary since i just need to query names??? (?? what did i mean by this)
+  known <- packages[packages %in% knownPackages & packages != "*"]
+  known <- lapply(known, renameKeywords) #TODO remove in JSON version
+  
+  whitelist <- getKnownBasePackages()
+  unknown <- packages[!(packages %in% knownPackages) & packages != "*" & !(packages %in% whitelist)]
+  
+  list(known = known, unknown = unknown)
 }
--- lib/tooling/nix.r
+++ lib/tooling/nix.r
@@ -1,61 +1,44 @@
 # TODO Ok so i really do need to have recurive dependency handling, is my stuff sufficient, or do i need to pivot to makescope?
-# TODO wtf is going on here
-formatPackageNix <- function(name, version, sha256, depends, imports, linkingTo, knownPackages) {
-  strip <- function(s) { if (is.na(s)) "" else gsub("[ \t\n]+", "", s) }
-  mkDepStr <- function(depends, imports, linkingTo, knownPackages){
-    depends <- paste( strip(depends), strip(imports), strip(linkingTo), sep=",")
-    depends <- unlist(strsplit(depends, split=",", fixed=TRUE))
-    depends <- lapply(depends, gsub, pattern="([^ \t\n(]+).*", replacement="\\1")
-    depends <- lapply(depends, gsub, pattern=".", replacement="_", fixed=TRUE)
-    depends <- depends[depends %in% knownPackages] #TODO probably need to run a bootstrap step - but actually that should be unnecessary since i just need to query names???
-    depends <- lapply(depends, function(d) ifelse(d == "import", "r_import", d))#TODO
-    depends <- paste(depends)
-    depends <- paste(sort(unique(depends)), collapse=" ")
-  }
-  
-  name <- ifelse(name == "import", "r_import", name) #TODO is this special cased - oh, its probably because import is a keyword - fix this by storing json and document it as a breaking fix
-  attr <- gsub(".", "_", name, fixed=TRUE) #TODO nix_escape_attr function? #TODO is this necessary if i use quoted stuff?
+formatPackageNix <- function(name, version, sha256, deps) {
+  name <- ifelse(name == "import", "r_import", name) #TODO instances of this need to be tested #TODO is this special cased - oh, its probably because import is a keyword - fix this by storing json and document it as a breaking fix
+  attr <- gsub(".", "_", name, fixed=TRUE) #we operate using the original names until this point in the file. #Todo what about collisions? use a proper prefix code #TODO nix_escape_attr function? #TODO is this necessary if i use quoted stuff?
   options(warn=5) #TODO docs say this only goes up to 2?
   
-  depends <- mkDepStr(depends, imports, linkingTo, knownPackages)
-  sprintf('    %s = derive2 { name = "%s"; version = "%s"; sha256 = "%s"; depends = [ %s ]; };', attr, name, version, sha256, depends)
+  known <- paste(deps$known, collapse = " ")
+  unknown <- sprintf('"%s"', paste0(deps$unknown, collapse='" "')) #because recycling
+  unknown <- ifelse(unknown == '""', "", unknown)
+  sprintf('    %s = derive2 { name = "%s"; version = "%s"; sha256 = "%s"; depends = [ %s ]; unknown = [ %s ]; };', attr, name, version, sha256, known, unknown)
 }
 
-
-genNix <- function(mirror, mirrors, snapshotDate, biocVersion, pkgs, knownPackages){
-  mode <- "nix"
-  if (mirror == "cran") {
-    deriveStr <- paste0(' snapshot = "', paste(snapshotDate), '"; ')
-  } else if (mirror == "irkernel") {
-    deriveStr <- ""
-  } else {
-    deriveStr <- paste0(' biocVersion = "', biocVersion, '"; ') #TODO put the mirror file in the output as well
-  }
+#TODO missing irkernel?? , how was the old code even supposed to get to irkernel, it would have errored on setting mirrorType
+genNix <- function(mirror, snapshotDate, biocVersion, pkgs, knownPackages){  mode <- "nix"
+  deriveStr <- if (mirror$name == "cran") {
+      sprintf('snapshot = "%s";', paste(snapshotDate))
+    } else if (startsWith(mirror$name, "bioc")) {
+      sprintf('biocVersion = "%s";', biocVersion) #TODO put the mirror file in the output as well
+    } else {""} #TODO actually probably error if no deriver attrs get set, are there any defalts? - might become unnecessary when i fix derivers to use the json db as well though?, remove mirror url generation complexity from here to separate url updater tool? -> leads to too much static exteralized state and needs more pipelining? -> ehh unix philoshopy?
   
-  pkgs <- genAllHashes(pkgs, mirror, mirrors)
+  pkgs <- addHashes(pkgs, mirror)
   
-  nix <- datmap(pkgs, function(p) { formatPackageNix(p$Package, p$Version, p$sha256, p$Depends, p$Imports, p$LinkingTo, knownPackages) })
-  wat <- sprintf("%s\n", paste(nix, collapse="\n")) #TODO fix indentation #TODO unfuck
+  wat <- datmap(pkgs, function(p) {
+    deps <- toDeps(p[c("Depends", "Imports", "LinkingTo")], knownPackages);
+    formatPackageNix(p$Package, p$Version, p$sha256, deps)
+    })
+  entries <- sprintf("%s\n", paste(wat, collapse="\n")) #TODO fix indentation #TODO unfuck
+  
+  command <- paste0(commandArgs(), collapse=" ") #TODO escaping? #TODO get tihs from a var since the beginning uses it too?
+  packagesFile <- sprintf("%s-packages.%s", mirror$name, mode)
   
   template <- readFile(paste0(sourceDir, "/packages-template.txt"))
   #Use %1$ style formatters to access the arguments.
-  command <- paste0(commandArgs(), collapse=" ") #TODO escaping?
-  packagesFile <- sprintf("%s-packages.%s", mirror, mode)
-  sprintf(template, command, packagesFile, deriveStr, wat)
+  sprintf(template, command, packagesFile, deriveStr, entries)
 }
 
-nixprefetchcached <- function(name, version, mirror, mirrors, mode="nix"){ #But why
-  getCached <- function(name, version, mirror, mirrors){
-    packagesFile <- sprintf("%s-packages.%s", mirror, mode)
-    if (file.exists(packagesFile)){
-      readFormatted <- as.data.table(read.table(skip=8, sep='"', text=head(readLines(packagesFile), -1)))
-      result <- as.character(readFormatted$V6[ readFormatted$V2 == name & readFormatted$V4 == version ])
-    }
+nixprefetchcached <- function(name, version, mirror){ #But why #TODO but why what
+  mode <- "nix"
+  packagesFile <- sprintf("%s-packages.%s", mirror$name, mode)
+  if (file.exists(packagesFile)){
+    readFormatted <- as.data.table(read.table(skip=8, sep='"', text=head(readLines(packagesFile), -1)))
+    result <- as.character(readFormatted$V6[ readFormatted$V2 == name & readFormatted$V4 == version ])
   }
-  
-  result <- getCached(name, version, mirror, mirrors)
-  if (length(result) == 0){ 
-    result <- genNixHash(name, version, mirror, mirrors)
-  }
-  result
 }
